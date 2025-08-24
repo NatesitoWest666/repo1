e4c17e34c978609f1e2ed2f628d33894dffdf38d
NatesitoWest666 <surcisur2005@gmail.com>
2025-08-24 15:09:44 -0400

    multi

--- re1.py
+++ re1.py
@@ -1,6 +1,9 @@
-a = int(input("Numero 1: "))
-b = int(input("Numero 2: "))
+a=int(input("Numero 1: "))
+b=int(input("Numero 2: "))
 
-suma = a + b
-print("La suma total es =", suma)
-print ("hola mundo")
+suma=a+b
+print("La suma total es: ", suma)
+print("hola mundo")
+
+multi=a*b
+print("la multiplicacion de a y b es: ", multi)
