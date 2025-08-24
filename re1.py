3ccabe1ebcdf91ea27d77d4517784f3381b8e03f
diegoruizg-byte <diego.ruiz.g@ucb.edu.bo>
2025-08-24 16:33:54 -0400

    hola a todos

---
+++ re1.py
@@ -0,0 +1,17 @@
+a=int(input("Numero 1: "))
+b=int(input("Numero 2: "))
+
+suma=a+b
+print("La suma total es: ", suma)
+print("hola mundo")
+
+multi=a*b
+print("la multiplicacion de a y b es: ", multi)
+
+division=a/b
+print("la division de a y b es: ", division)
+
+resta=a-b
+print("la resta de a y b es: ", resta)
+
+print("Hola a todos ")
